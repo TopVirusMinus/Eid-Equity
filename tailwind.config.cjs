5ee45aef69fe5853991c06ba1b9b90b6ac91fb75
TopVirusMinus <mwmma5000@gmail.com>
2023-06-10 04:41:49 +0300

    Added Strategy Section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      animation: {
+        slideIn: "slideIn 0.5s ease-in",
+      },
+    },
     plugins: [],
     colors: {
-      secondary: "#0093FF",
+      secondary: "#005694",
+      secondaryLight: "#0093FF",
       mask: "rgba(6, 32, 54, 0.85)",
       white: "white",
       black: "black",
+      blackish: "#081c1f",
+      myGold: "#daa521",
     },
   },
 };
